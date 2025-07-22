a9a394605c68359149aedbf701ba97e9db97619f
JaviGomezCba <nicogomezcordoba@hotmail.com>
2025-07-22 16:25:04 -0300

    Actualizar URLs backend a producción

--- mi-ecommerce-react/src/components/ContactForm.jsx
+++ mi-ecommerce-react/src/components/ContactForm.jsx
@@ -37,7 +37,7 @@ const ContactForm = ({ innerRef }) => {
     if (!validar()) return
 
     try {
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch('https://back-ecommerce-m1kv.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
